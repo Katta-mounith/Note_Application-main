2c08c6a8a60b5f06ec31ac01444c63cec1942ad7
Katta-mounith <54463229+Katta-mounith@users.noreply.github.com>
2022-12-14 21:08:40 -0500

    final commit

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 # Note_Application
 ERD Explain for Note application.
-![Screenshot_20221205_213112](https://user-images.githubusercontent.com/54463229/205794385-a39b0ad8-76fa-4f92-b731-37264f5514bd.png)
-
+[Blank diagram.pdf](https://github.com/Katta-mounith/Note_Application/files/10043450/Blank.diagram.pdf)
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 const userRoutes = require('./server/routes/user');
-
+const noteRoutes = require('./server/routes/note');
 app.use(express.json());
 
 //CORS middleware
@@ -14,6 +14,7 @@ app.use(function(req, res, next) {
 });
 
 app.use("/users", userRoutes);
+app.use("/notes", noteRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}!`));
