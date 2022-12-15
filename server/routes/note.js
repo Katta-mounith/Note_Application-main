00f1266b01d0dc5baf294b41ce6087e07c86bc6a
Katta-mounith <54463229+Katta-mounith@users.noreply.github.com>
2022-12-14 21:06:53 -0500

    final commit

---
+++ server/routes/note.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const Note = require('../models/note');
+const router = express.Router();
+
+router
+  .get('/', async (req, res) => {
+    try {
+      const notes = await Note.getAllNotes();
+      res.send(notes);
+    } catch(err) {
+      res.status(401).send({message: err.message});
+    }
+  })
+  .post('/getNote', async (req, res) => {
+    try {
+      let note = await Note.getNote(req.body);
+      res.send({...note})
+    } catch(err) {
+      res.status(401).send({message: err.message});
+    }
+  })
+
+  .post('/create', async (req, res) => {
+    try {
+      console.log(req.body)
+      let note = await Note.createNote(req.body);
+      res.send({...note})
+    } catch(err) {
+      res.status(401).send({message: err.message});
+    }
+  })
+  .post('/Read', async (req, res) => {
+    try {
+      let note = await Note.Read(req.body);
+      res.send({...note,noteContent})
+    } catch(err) {
+      res.status(401).send({message: err.message});
+    }
+  })
+  .put('/edit', async (req, res) => {
+    try {
+      let note = await Note.editNotes(req.body);
+      res.send({...note, noteContent});
+    } catch(err) {
+      res.status(401).send({message: err.message})
+    }
+  })
+
+  .delete('/delete', async (req, res) => {
+    try {
+      Note.deleteNote(req.body);
+      res.send({success: "We'll Miss You... :("})
+    } catch(err) {
+      res.status(401).send({message: err.message})
+    }
+  })
+module.exports = router;
--- server/routes/user.js
+++ server/routes/user.js
@@ -27,6 +27,7 @@ router
       res.send({...user, password: undefined})
     } catch(err) {
       res.status(401).send({message: err.message});
+      return;
     }
   })
 
@@ -47,35 +48,5 @@ router
       res.status(401).send({message: err.message})
     }
   })
-
-
-
-  
 module.exports = router;
-/*const express = require('express');
-const User = require('../models/user');
-const router = express.Router();
 
-router
-  .get('/', async (req, res) => {
-    try {
-      const users = await User.getAllUsers();
-      res.send(users);
-    } catch(err) {
-      res.status(401).send({message: err.message});
-    }
-  })
-
-  .post('/login', async (req, res) => {
-    try {
-      let user = await User.login(req.body);
-      res.send({...user, password: undefined})
-    } catch(err) {
-      res.status(401).send({message: err.message});
-    }
-  })
-
-
-  
-module.exports = router;
-*/
